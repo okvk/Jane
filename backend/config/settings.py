34d198cc9b52b6cb206f7fe06ca8809cb883aaf2
ShaobiaoLin <octobug@qq.com>
2019-12-03 22:13:32 +0800

    Bugfix/clean browser session cookie (#120)
    * fix: unuse rest_framework.authentication.SessionAuthentication
    * refactor: rewrite frontend error tips
    * chore: retry travis failed check automatically
    * chore: travis builds master & develop & pull request only

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -98,7 +98,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
 }
