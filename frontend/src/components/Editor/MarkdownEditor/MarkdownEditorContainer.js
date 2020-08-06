ea0a1cd09cfa4190980dc7f4f38630f3755f3c2c
Octobug <whalevocal@gmail.com>
2020-08-06 23:42:59 +0800

    feat(vditor): replace custome editor with vditor #136

--- frontend/src/components/Editor/MarkdownEditor/MarkdownEditorContainer.js
+++ frontend/src/components/Editor/MarkdownEditor/MarkdownEditorContainer.js
@@ -4,13 +4,21 @@ import PropTypes from "prop-types";
 import Vditor from "vditor";
 import "vditor/src/assets/scss/index.scss";
 
-import MarkdownEditor from "./MarkdownEditor";
 import "./MarkdownEditor.scss";
 
 const classNames = require("classnames");
 
 class MarkdownEditorContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.vditor = null;
+    this.state = {
+      vditorReady: false
+    };
+  }
+
   componentDidMount() {
+    const thisCPN = this;
     const vditor = new Vditor("vditor", {
       toolbarConfig: {
         pin: true
@@ -19,53 +27,37 @@ class MarkdownEditorContainer extends React.PureComponent {
         enable: false
       },
       after() {
-        vditor.setValue("");
+        thisCPN.setState({ vditorReady: true });
       }
     });
+    thisCPN.vditor = vditor;
+    this.props.setVditor(vditor);
+  }
+
+  componentDidUpdate() {
+    const { editorState } = this.props;
+    if (this.state.vditorReady) {
+      this.vditor.setValue(editorState);
+    }
   }
 
   render() {
-    const { editorState, preview } = this.props;
     return (
       <div className="markdown-editor">
-        <div
-          className={classNames("editor-pane", {
-            "editor-preview-mode": preview
-          })}
-        >
+        <div className={classNames("editor-pane")}>
           <div id="vditor" />
-          {/* <Editor
-            value={editorState}
-            onChange={handleEditorChange}
-            theme={theme}
-          /> */}
         </div>
-        {preview ? (
-          <div className="result-pane">
-            <MarkdownEditor
-              className="result"
-              skipHtml={false}
-              escapeHtml={false}
-              src={editorState}
-            />
-          </div>
-        ) : null}
       </div>
     );
   }
 }
 
 MarkdownEditorContainer.propTypes = {
-  editorState: PropTypes.string,
-  handleEditorChange: PropTypes.func.isRequired,
-  preview: PropTypes.bool,
-  theme: PropTypes.string
+  editorState: PropTypes.string
 };
 
 MarkdownEditorContainer.defaultProps = {
-  editorState: "",
-  preview: false,
-  theme: "monokai"
+  editorState: ""
 };
 
 export default MarkdownEditorContainer;
